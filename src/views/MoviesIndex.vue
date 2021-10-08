10caa6f54bc077acfc58a2ad21117bf93a53419e
Nate <natelalvarado@gmail.com>
2021-10-08 11:48:51 -0400

    Adds CRUD routes

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="movies-index">
+    <h1>All Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }}</h2>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <p>Director: {{ movie.director }}</p>
+      <router-link :to="`/movies/${movie.id}`">See Details</router-link>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    axios.get("/movies").then((response) => {
+      console.log(response.data);
+      this.movies = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
---
+++ src/views/MoviesNew.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="movies-new">
+    <form v-on:submit.prevent="createMovie()">
+      <h1>New Movie</h1>
+      <div>
+        Title:
+        <input type="text" v-model="newMovieParams.title" />
+      </div>
+      <div>
+        Year:
+        <input type="text" v-model="newMovieParams.year" />
+      </div>
+      <div>
+        Plot:
+        <input type="text" v-model="newMovieParams.plot" />
+      </div>
+      <div>
+        Director:
+        <input type="text" v-model="newMovieParams.director" />
+      </div>
+      <input type="submit" value="Create" />
+    </form>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      newMovieParams: {},
+      errors: [],
+    };
+  },
+  created: function () {},
+  methods: {
+    createMovie: function () {
+      axios
+        .post("/movies", this.newMovieParams)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/movies");
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+  },
+};
+</script>
