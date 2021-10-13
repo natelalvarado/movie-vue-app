68f5a23492e506f4872085f2db1b1e7d350c7ba2
Nate <natelalvarado@gmail.com>
2021-10-13 11:30:04 -0400

    Adds sorting and filtering for all movies page

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const routes = [
   {
     path: "/movies",
     name: "movies-index",
-    componet: MoviesIndex,
+    component: MoviesIndex,
   },
 
   {
--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="movies-index">
     <h1>All Movies</h1>
-    <div v-for="movie in movies" v-bind:key="movie.id">
+    <h1>All Movies</h1>
+    Search by title:
+    <input v-model="titleFilter" list="titles" />
+    <datalist id="titles">
+      <option v-for="movie in movies" v-bind:key="movie.id">
+        {{ movie.title }}
+      </option>
+    </datalist>
+    <div>
+      <button v-on:click="setSortAttribute('title')">
+        Sort Alphabetically
+      </button>
+    </div>
+
+    <div
+      v-for="movie in orderBy(
+        filterBy(movies, titleFilter, 'title'),
+        sortAttribute,
+        sortOrder
+      )"
+      v-bind:key="movie.id"
+    >
       <h2>{{ movie.title }}</h2>
       <p>Year: {{ movie.year }}</p>
       <p>Plot: {{ movie.plot }}</p>
@@ -15,11 +36,16 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
       movies: [],
+      titleFilter: "",
+      sortAttribute: "title",
+      sortOrder: 1,
     };
   },
   created: function () {
@@ -28,6 +54,15 @@ export default {
       this.movies = response.data;
     });
   },
-  methods: {},
+  methods: {
+    setSortAttribute: function (attribute) {
+      if (this.sortAttribute === attribute) {
+        this.sortOrder = this.sortOrder * -1;
+      } else {
+        this.sortAttribute = attribute;
+        this.sortOrder = 1;
+      }
+    },
+  },
 };
 </script>
